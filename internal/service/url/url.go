f8d4c26d505f57e61848176d59e0613f297d66be
zhanbolat <z.yerkinbay@slotegrator.space>
2024-01-12 15:48:50 +0100

    fix

--- internal/service/url/url.go
+++ internal/service/url/url.go
@@ -20,7 +20,7 @@ func New(log *slog.Logger) *Url {
 }
 
 func (this *Url) Validate(domain string, urls []string) map[string]int {
-	const op = "service.url.validate"
+	const op = "service.url.Validate"
 
 	domain = strings.TrimRight(domain, "/")
 
@@ -29,21 +29,24 @@ func (this *Url) Validate(domain string, urls []string) map[string]int {
 		slog.String("domain", domain),
 	)
 
+	log.Info("starting validate urls")
+
 	var wg sync.WaitGroup
-	var validated sync.Map
+	var validated sync.Map // add only string => int
+	results := make(map[string]int)
 
-	for _, originalUrl := range urls {
-		url := "/" + strings.Trim(originalUrl, "/")
+	for _, url := range urls {
+		url = "/" + strings.Trim(url, "/")
 
-		if _, ok := validated.Load(url); !ok {
+		if _, ok := results[url]; !ok {
+			results[url] = -1
 
-			log.Info("start validate " + url)
 			wg.Add(1)
 
 			go func(url string) {
 				defer wg.Done()
 
-				status, err := this.fetchURLStatus(domain + url)
+				status, err := this.fetchUrlStatus(domain + url)
 				if err != nil {
 					log.Error("failed validate "+url, sl.Err(err))
 					validated.Store(url, -1)
@@ -51,14 +54,14 @@ func (this *Url) Validate(domain string, urls []string) map[string]int {
 				}
 
 				validated.Store(url, status)
-				log.Info("finish validate " + url)
 			}(url)
 		}
 	}
 
 	wg.Wait()
 
-	results := make(map[string]int)
+	log.Info("finished validate urls")
+
 	validated.Range(func(key, value any) bool {
 		results[key.(string)] = value.(int)
 
@@ -68,17 +71,24 @@ func (this *Url) Validate(domain string, urls []string) map[string]int {
 	return results
 }
 
-func (this *Url) fetchURLStatus(fullUrl string) (int, error) {
+func (this *Url) fetchUrlStatus(fullUrl string) (int, error) {
+	const op = "service.url.fetchUrlStatus"
+
+	log := this.log.With(
+		slog.String("op", op),
+		slog.String("fullUrl", fullUrl),
+	)
+
 	resp, err := http.Get(fullUrl)
 
 	if err != nil {
 		return 0, err
 	}
 
-	defer func(Body io.ReadCloser) {
+	func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			this.log.Error("failed close body", sl.Err(err))
+			log.Error("failed close body", sl.Err(err))
 		}
 	}(resp.Body)
 
